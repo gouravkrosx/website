1c42c78b0ee90482ad6eafefbccf43fe3b6e0043
Animesh Pathak <kurosakiichigo.songoku@gmail.com>
2024-10-22 17:24:37 +0530

    fix: neon cta design fix
    Signed-off-by: Animesh Pathak <kurosakiichigo.songoku@gmail.com>

---
+++ prebuild.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Replace these with your actual submodule repositories and paths
+git clone https://github.com/keploy/demo-projects.git ./components/atg/demo-projects
+# Add more git clone commands for other submodules if needed
